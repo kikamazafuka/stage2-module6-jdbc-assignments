88dc6b5c89a8b9563bfaf814cab5a0ff2e59bab1
Artur Semenas <uncelfuka@gmail.com>
2024-02-22 21:02:36 +0100

    Module #3. Task #2. JDBC

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,9 +5,21 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
+
+    private static Properties properties = new Properties();
+    private final CustomConnector customConnector;
     private static volatile CustomDataSource instance;
     private final String driver;
     private final String url;
@@ -15,11 +27,84 @@ public class CustomDataSource implements DataSource {
     private final String password;
 
     private CustomDataSource(String driver, String url, String password, String name) {
+        this.driver = driver;
+        this.url = url;
+        this.name = name;
+        this.password = password;
+        this.customConnector = new CustomConnector();
+
     }
 
     public static CustomDataSource getInstance() {
-
+        try {
+            properties.load(new FileInputStream("app.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String driver = properties.getProperty("postgres.driver");
+        String url = properties.getProperty("postgres.url");
+        String username = properties.getProperty("postgres.name");
+        String password = properties.getProperty("postgres.password");
+        if (instance == null) {
+            synchronized (CustomDataSource.class) {
+                if (instance == null) {
+                    instance = new CustomDataSource(
+                            driver,
+                            url,
+                            username,
+                            password);
+                }
+            }
+        }
         return instance;
     }
 
+    @Override
+    public Connection getConnection() throws SQLException {
+        try {
+            return customConnector.getConnection(url, name, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return customConnector.getConnection(url, username, password);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
 }
--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,9 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,28 +20,170 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "";
-    private static final String updateUserSQL = "";
-    private static final String deleteUser = "";
-    private static final String findUserByIdSQL = "";
-    private static final String findUserByNameSQL = "";
-    private static final String findAllUserSQL = "";
+    private static final String createUserSQL = "insert into myusers (id, firstname, lastname, age) values (?,?,?,?)";
+    private static final String updateUserSQL = "update myusers set id=?, firstname=?, lastname=?, age=?";
+    private static final String deleteUser = "delete from myusers where id=?";
+    private static final String findUserByIdSQL = "select * from myusers where id=?";
+    private static final String findUserByNameSQL = "select * from myusers where firstname=?";
+    private static final String findAllUserSQL = "select * from myusers";
+
 
     public Long createUser() {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        User user = new User();
+        try {
+            ps = connection.prepareStatement(createUserSQL, Statement.RETURN_GENERATED_KEYS);
+            int i = 0;
+            ps.setLong(++i, user.getId());
+            ps.setString(++i, user.getFirstName());
+            ps.setString(++i, user.getLastName());
+            ps.setInt(++i, user.getAge());
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()){
+                user.setId(rs.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            closeResources();
+        }
+        return user.getId();
+
     }
 
     public User findUserById(Long userId) {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps = connection.prepareStatement(findUserByIdSQL);
+            ps.setLong(1, userId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return mapResultSetToUser(rs);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            closeResources();
+        }
+        throw new IllegalArgumentException("User not found");
     }
 
     public User findUserByName(String userName) {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps = connection.prepareStatement(findUserByNameSQL);
+            ps.setString(1, userName);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return mapResultSetToUser(rs);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            closeResources();
+        }
+        throw new IllegalArgumentException("User not found");
     }
 
     public List<User> findAllUser() {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        List<User> users = new ArrayList<>();
+        try {
+            ps = connection.prepareStatement(findAllUserSQL);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                User user = mapResultSetToUser(rs);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            closeResources();
+        }
+        return users;
     }
 
-    public User updateUser() {
+    public User updateUser(User user) {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps = connection.prepareStatement(updateUserSQL);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setInt(3, user.getAge());
+            ps.setLong(4, user.getId());
+            ps.executeUpdate();
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Can't update user");
+        } finally {
+            closeResources();
+        }
+
     }
 
     private void deleteUser(Long userId) {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            ps = connection.prepareStatement(deleteUser);
+            ps.setLong(1, userId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("User not found");
+        } finally {
+            closeResources();
+        }
+    }
+    private User mapResultSetToUser(ResultSet resultSet) throws SQLException {
+        return new User(
+                resultSet.getLong("id"),
+                resultSet.getString("firstname"),
+                resultSet.getString("lastname"),
+                resultSet.getInt("age")
+        );
+    }
+
+    private void closeResources() {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
